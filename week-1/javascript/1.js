ff7844e65e91a86ea29ce4f5c886dd4dbec56040
Christine Ocana <ocanachristine43@gmail.com>
2024-05-29 16:26:16 +0800

    [JS-1] Check a number whether it is true or false.

--- week-1/javascript/1.js
+++ week-1/javascript/1.js
@@ -3,3 +3,9 @@
  * @param {number} num The number to check.
  * @return {boolean} True if the number is even, false if it is odd.
  */
+
+const num = 3;
+
+const checkNumber = (num %2 === 0) ? true : false;
+
+console.log(checkNumber);
