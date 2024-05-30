eb3d60e9d823af0c9ef59a16be3e17e42c3cb91e
Christine Ocana <ocanachristine43@gmail.com>
2024-05-30 11:22:17 +0800

    [JS-7] Write a function that takes a number as a parameter and throws a custom 'Error' if the number is not an integer.

--- week-1/javascript/7.js
+++ week-1/javascript/7.js
@@ -3,3 +3,20 @@
  * @param {number} num The number to check.
  * @throws {Error} If the number is not an integer.
  */
+
+
+function number(n){
+
+    if (!Number.isInteger(n)){
+        throw new Error("Not a number")
+    } else {
+        return n
+    }
+}
+
+try {
+    const checkNumber = number("Christine");
+    console.log(checkNumber + " is a number.");
+} catch (error) {
+    console.log ( error.message)
+}
