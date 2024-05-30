760c04af4cbf07a3cce3bd0e736afaad8d25c87d
Christine Ocana <ocanachristine43@gmail.com>
2024-05-30 14:16:40 +0800

    [JS-10]Write a function that accepts 2 numbers as parameters and throws a custom 'Error' if the second number is 0

--- week-1/javascript/10.js
+++ week-1/javascript/10.js
@@ -4,3 +4,17 @@
  * @param {number} num2 The second number to check.
  * @throws {Error} If the second number is 0.
  */
+
+
+function numbers(num1, num2){
+    if(num1 > 0 && num2 === 0){
+        throw new Error("Error: The second number is zero.")
+    } return (num1 + " and " + num2)
+}
+
+try {
+    const checkNumber = numbers(1, 0);
+    console.log(checkNumber);
+}catch (error){
+    console.log(error.message);
+}
