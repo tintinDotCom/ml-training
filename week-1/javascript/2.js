d53d259ebc4db87e18dca74b650c33c27beb1fb2
Christine Ocana <ocanachristine43@gmail.com>
2024-05-30 08:48:01 +0800

    [JS-2] Sum up all the numbers in the array.

--- week-1/javascript/2.js
+++ week-1/javascript/2.js
@@ -3,3 +3,18 @@
  * @param {number[]} arr The array of numbers.
  * @return {number} The sum of all numbers in the array.
  */
+
+
+function sum(arr) {
+    let total = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        total += arr[i]
+    };
+
+    return total;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+const totalSum = sum(numbers);
+console.log(totalSum)
