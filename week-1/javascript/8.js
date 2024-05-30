b76092b4b6c955a8528179bfa74b0c4aa64fb702
Christine Ocana <ocanachristine43@gmail.com>
2024-05-30 13:51:02 +0800

    [JS-8]Write a function that returns all vowels in a string.

--- week-1/javascript/8.js
+++ week-1/javascript/8.js
@@ -1,5 +1,22 @@
 /**
  * Write a function that returns all vowels in a string.
  * @param {string} str The string to check.
- * @return {string} The string with all vowels removed.
+ * @return {string} The string with all vowels.
  */
+
+function getVowels(str){
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let string = '';
+    
+    for (let char of str.toLowerCase()){
+        if(vowels.includes(char)){
+            string += char;
+        }
+    } 
+    return string;
+    
+}
+
+const stringInput = 'Christine Mae';
+const vowels = getVowels(stringInput);
+console.log(vowels);
