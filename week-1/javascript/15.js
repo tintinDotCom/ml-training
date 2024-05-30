b4fed84d1f1c952e4ce367e2fcba38b0d5dae3e0
Christine Ocana <ocanachristine43@gmail.com>
2024-05-30 16:08:29 +0800

    [JS-15]Write a function for loop that iterates from 1 to a given number. It then checks and outputs whether the number is even or odd.

--- week-1/javascript/15.js
+++ week-1/javascript/15.js
@@ -2,3 +2,14 @@
  * Write a function for loop that iterates from 1 to a given number. It then checks and outputs whether the number is even or odd.
  * @param {number} n The number to check.
  */
+
+
+function evenOrOdd(n){
+    for(let x = 0; x <= n; x++){
+        if(x %2 === 0){
+            console.log(`${x} is even`)
+        } else  console.log(`${x} is odd`)
+    }
+}
+
+evenOrOdd(21)
