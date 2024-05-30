687f10647a089fce989d51a1f1507c5f3c8a1068
Christine Ocana <ocanachristine43@gmail.com>
2024-05-30 15:17:10 +0800

    [JS-12] Write a function that takes an array of number and throws a custom error if the array is empty.

--- week-1/javascript/12.js
+++ week-1/javascript/12.js
@@ -3,3 +3,16 @@
  * @param {number[]} arr The array to check.
  * @throws {Error} If the array is empty.
  */
+
+function Numbers(arr) {
+    if(arr.length === 0) {
+        throw new Error("Array is empty.")
+    } return arr;
+}
+
+try{
+    const numbers = Numbers([]);
+    console.log(numbers)
+}catch(error){
+    console.log(error.message);
+}
