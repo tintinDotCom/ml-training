be3de8bc086ae36d3d87fd8e76f6e6cd796b444a
Christine Ocana <ocanachristine43@gmail.com>
2024-05-31 10:54:23 +0800

    [JS-18]Create a shopping cart that allows the user to add items to the cart and remove items from the cart.

--- week-1/javascript/18.js
+++ week-1/javascript/18.js
@@ -16,3 +16,56 @@
  * @param {string[]} items
  * @returns {string}
  */
+
+
+function createShoppingCart() {
+    const cart = [];
+  
+    return {
+      addItem: (item) => {
+
+        if (cart.length === 10) {
+          return "Cart is full, cannot add more items.";
+        }
+
+        cart.push(item);
+        return `Added ${item.name} to the cart.`;
+      },
+      removeItem: (itemName) => {
+        const itemIndex = cart.findIndex((item) => item.name === itemName);
+        if (itemIndex === -1) {
+          return `${itemName} is not in the cart.`;
+        }
+  
+        const removedItem = cart.splice(itemIndex, 1)[0];
+        return `Removed ${removedItem.name} from the cart.`;
+      },
+      getTotalPrice: () => {
+        return cart.reduce((total, item) => total + item.price, 0);
+      },
+      getTotalItems: () => {
+        return cart.length;
+      },
+    };
+  }
+  
+
+  const cart = createShoppingCart();
+  
+console.log(cart.addItem({ name: "apple", price: 10 })); 
+console.log(cart.addItem({ name: "banana", price: 12 })); 
+console.log(cart.addItem({ name: "orange", price: 15 })); 
+console.log(cart.addItem({ name: "grape", price: 200 })); 
+console.log(cart.addItem({ name: "pear", price: 50 })); 
+console.log(cart.addItem({ name: "kiwi", price: 60 })); 
+console.log(cart.addItem({ name: "mango", price: 80 })); 
+console.log(cart.addItem({ name: "peach", price: 60 })); 
+console.log(cart.addItem({ name: "plum", price: 80 })); 
+console.log(cart.addItem({ name: "strawberry", price: 100 })); 
+console.log(cart.addItem({ name: "blueberry", price: 200 })); 
+
+
+console.log('Total items in the cart')
+console.log(cart.getTotalItems()); 
+console.log('Total price')
+console.log(cart.getTotalPrice()); 
