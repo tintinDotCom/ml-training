cac99a33bea797848a4ce8b86fb50df77fd66063
Christine Ocana <ocanachristine43@gmail.com>
2024-05-31 16:11:17 +0800

    [TS-3] FizzBuzz

--- week-1/typescript/3.ts
+++ week-1/typescript/3.ts
@@ -5,3 +5,25 @@
  * For numbers which are multiples of both three and five print 'FizzBuzz'.
  * @param {number} n The number to print.
  */
+
+function FizzBuzz(n: number): void{
+    for (let x : number = 1; x < n; x++) {
+        if(x %3 === 0 && x %5 === 0){
+            console.log('FizzBuzz')
+        } 
+        else if(x %3 === 0){
+            console.log('Fizz')
+        }
+        else if(x %5 === 0){
+            console.log('Buzz')
+        }
+        else{
+            console.log(x)
+        }
+        
+    }
+}
+
+console.log('====================================');
+FizzBuzz(20);
+console.log('====================================');
