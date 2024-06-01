81099dfbc9bf7bb1fbecd07996eb4dda8f7482ce
Christine Ocana <ocanachristine43@gmail.com>
2024-06-01 08:27:41 +0800

    [TS-8]write a function that returns all vowels in a string

--- week-1/typescript/8.ts
+++ week-1/typescript/8.ts
@@ -1,5 +1,23 @@
 /**
  * Write a function that returns all vowels in a string.
  * @param {string} str The string to check.
- * @return {string} The string with all vowels removed.
+ * @return {string} The string with all vowels.
  */
+
+function GetVowels (str: string) {
+    const vowels : string[] = ['a', 'e', 'i', 'o', 'u'];
+    let vowelString : string = '';
+
+    for (let char of str.toLowerCase()) {
+        if(vowels.includes(char)){
+            vowelString += char;
+        }
+    }
+    return vowelString;
+}
+
+const inputString : string = 'Christine';
+const Vowels : string = GetVowels(inputString);
+console.log('====================================');
+console.log(Vowels);
+console.log('====================================');
