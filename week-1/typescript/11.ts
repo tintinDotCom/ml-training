432e5813ca0544277ce25d7e7e94a999663ccc94
Christine Ocana <ocanachristine43@gmail.com>
2024-06-01 08:43:11 +0800

    [TS-11]Write a function that converts HTML entities to their corresponding characters.

--- week-1/typescript/11.ts
+++ week-1/typescript/11.ts
@@ -4,11 +4,15 @@
  * @return {string} The string with HTML entities converted to their corresponding characters.
  */
 
-// ENTITY OBJECT
-const entities = {
-  "&": "&amp;",
-  "<": "&lt;",
-  ">": "&gt;",
-  '"': "&quot;",
-  "'": "&apos;",
-};
+
+function Entity(str : string): string{
+  return str.replace(/&lt;/g, `<`)
+            .replace(/&amp;/g, `&`)
+            .replace(/&gt;/g, `>`)
+            .replace(/&quot;/g, `"`)
+            .replace(/&apos;/g, `'`);
+}
+
+console.log('====================================');
+console.log(Entity('&lt; &amp; &gt; &quot; &apos;'));
+console.log('====================================');
