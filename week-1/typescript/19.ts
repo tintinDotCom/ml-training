0ea07c432318d8ecda70b449801d9a39201ae0b7
Christine Ocana <ocanachristine43@gmail.com>
2024-06-01 09:23:02 +0800

    [TS-19]Write a function that merges two arrays into a single array and removes duplicates.

--- week-1/typescript/19.ts
+++ week-1/typescript/19.ts
@@ -9,3 +9,11 @@
  * @param {number[]} arr2
  * @returns {number[]}
  */
+
+function MergeArrays(arr1 : number[], arr2 : number[]){
+    return(Array.from(new Set([...arr1, ...arr2].sort())))
+}
+
+
+
+console.log(MergeArrays([1,2,3,44,5], [3, 4,5,5,6,5]));
