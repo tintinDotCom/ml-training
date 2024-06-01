5f552a916cd95297321fcd1eb6349e199371ee99
Christine Ocana <ocanachristine43@gmail.com>
2024-06-01 08:46:02 +0800

    [TS-12]Write a function that takes an array of numbers and throws a custom 'Error' if the array is empty.

--- week-1/typescript/12.ts
+++ week-1/typescript/12.ts
@@ -3,3 +3,18 @@
  * @param {number[]} arr The array to check.
  * @throws {Error} If the array is empty.
  */
+function Num(arr : number[]): number[] {
+    if(arr.length === 0) {
+        throw new Error("Array is empty.")
+    } return arr;
+}
+
+try{
+    console.log('====================================');
+    console.log(Num([]));
+    console.log('====================================');
+} catch (error) {
+    console.log('====================================');
+    console.log((error as Error).message);
+    console.log('====================================');
+}
