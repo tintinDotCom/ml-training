02f831476a3a89a01e94e938a3edac229fd8c006
Christine Ocana <ocanachristine43@gmail.com>
2024-06-01 08:52:18 +0800

    [TS-14]Write a function that accepts an array of numbers and displays the highest and lowest numbers in the array.

--- week-1/typescript/14.ts
+++ week-1/typescript/14.ts
@@ -6,3 +6,17 @@
  * Example array: [5, 2, 8, 1, 9, 3]
  * Expected output: "The lowest number is 1 and the highest number is 9."
  */
+function num(arr : number[]){
+    if(arr.length === 0) {
+        return("Array is empty")
+    } 
+
+    const highestNumber : number = Math.max(...arr);
+    const lowestNumber : number = Math.min(...arr);
+
+    return(`The lowest number is ${lowestNumber} and the highest number is ${highestNumber}`)
+}
+
+console.log('====================================');
+console.log(num([1,4,65,78,90]));
+console.log('====================================');
