3331f524795c37b2f43185c709f8bf3be609ceb4
Christine Ocana <ocanachristine43@gmail.com>
2024-06-01 08:39:44 +0800

    [TS-10]Write a function that accepts 2 numbers as parameters and throws a custom 'Error' if the second number is 0

--- week-1/typescript/10.ts
+++ week-1/typescript/10.ts
@@ -4,3 +4,20 @@
  * @param {number} num2 The second number to check.
  * @throws {Error} If the second number is 0.
  */
+
+function Nums(num1: number, num2: number): number {
+    if (num1 >= 1 && num2 === 0) {
+        throw new Error('The second number is 0');
+    }
+    return num1 && num2;
+}
+
+try {
+    console.log('====================================');
+    console.log(Nums(1, 0));
+    console.log('====================================');
+}catch (error) {
+    console.log('====================================');
+    console.log((error as Error).message);
+    console.log('====================================');
+}
