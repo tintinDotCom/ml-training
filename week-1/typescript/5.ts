9746df21e48f83cac56e047d45e04c0ff2a17da7
Christine Ocana <ocanachristine43@gmail.com>
2024-06-01 08:02:55 +0800

    [TS-5]converts temperature in Celsius to Fahrenheit

--- week-1/typescript/5.ts
+++ week-1/typescript/5.ts
@@ -4,3 +4,15 @@
  * @param {number} celsius The temperature in Celsius.
  * @return {number} The temperature in Fahrenheit.
  */
+
+
+function CelsiusToFahrenheit(celsius : number) : void {
+    const answer : number = celsius * 9/5 + 32;
+
+    console.log('====================================');
+    console.log(answer);
+    console.log('====================================');
+}
+
+
+CelsiusToFahrenheit(60)
