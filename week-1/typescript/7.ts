5e8471b719d4f35ee3fa543d5ee151f23ba53cdf
Christine Ocana <ocanachristine43@gmail.com>
2024-06-01 08:18:50 +0800

    [TS-7]Write a function that takes a number as a parameter and throws a custom 'Error' if the number is not an integer.

--- week-1/typescript/7.ts
+++ week-1/typescript/7.ts
@@ -3,3 +3,21 @@
  * @param {number} num The number to check.
  * @throws {Error} If the number is not an integer.
  */
+
+function Num(num: number): number {
+    if (!Number.isInteger(num)) {
+        throw new Error(`${num} is not an integer.`);
+    }
+    return num;
+}
+
+try {
+    console.log('====================================');
+    console.log(Num(3));
+    console.log('====================================');
+
+} catch (error) {
+    console.log('====================================');
+    console.log((error as Error).message);
+    console.log('====================================');
+}
