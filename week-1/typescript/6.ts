a8590e516d15bab1a90c9434018fd066244ebad4
Christine Ocana <ocanachristine43@gmail.com>
2024-06-01 08:05:59 +0800

    [TS-6]converts temperature in fahrenheit to celsius

--- week-1/typescript/6.ts
+++ week-1/typescript/6.ts
@@ -4,3 +4,13 @@
  * @param {number} fahrenheit The temperature in Fahrenheit.
  * @return {number} The temperature in Celsius.
  */
+
+function FarenheitToCelsius(fahrenheit : number) : void {
+    const answer : number = (fahrenheit - 32) * 5/9;
+
+    console.log('====================================');
+    console.log(answer);
+    console.log('====================================');
+};
+
+FarenheitToCelsius(140)
