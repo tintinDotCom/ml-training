e84cb8379978507d593cf8930907aec2c7d5d8ba
Christine Ocana <ocanachristine43@gmail.com>
2024-06-01 09:25:30 +0800

    [TS-20]Write a function that accepts a string as input and swaps the case of each character in the string.

--- week-1/typescript/20.ts
+++ week-1/typescript/20.ts
@@ -7,3 +7,18 @@
  * @param {string} str
  * @returns {string}
  */
+
+
+
+function SwapCase(str : string){
+    let newTxt : string = '';
+
+    for (let index = 0; index < str.length; index++) {
+        newTxt += str[index] === str[index].toUpperCase() ? str[index].toLowerCase() : str[index].toUpperCase()
+    }
+
+
+    return newTxt;
+}
+
+console.log(SwapCase("the QUICK brown FOx"));
