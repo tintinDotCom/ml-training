0ad0c9484d0ea07804e59c8475731ebe8e724265
Christine Ocana <ocanachristine43@gmail.com>
2024-06-01 09:05:38 +0800

    [TS-16]Write a function that computes the average marks of the following students

--- week-1/typescript/16.ts
+++ week-1/typescript/16.ts
@@ -15,10 +15,36 @@
  * @return {number} The average marks of the students.
  */
 
-const students = [
-  { name: "John", mark: 90 },
-  { name: "Jane", mark: 85 },
-  { name: "Joe", mark: 95 },
-  { name: "Tom", mark: 65 },
-  { name: "Nancy", mark: 75 },
-];
+function Average() : number {
+  const students = [
+    { name: "John", mark: 90 },
+    { name: "Jane", mark: 85 },
+    { name: "Joe", mark: 95 },
+    { name: "Tom", mark: 95 },
+    { name: "Nancy", mark: 75 },
+  ];
+
+  let totalMarks : number = 0;
+  for (let student of students) {
+    let grade : string;
+    if (student.mark < 60) {
+      grade = 'F';
+    } else if (student.mark >= 60 && student.mark <= 69) {
+      grade = 'D';
+    } else if (student.mark >= 70 && student.mark <= 79) {
+      grade = 'C';
+    } else if (student.mark >= 80 && student.mark <= 89) {
+      grade = 'B';
+    } else {
+      grade = 'A';
+    }
+    console.log(`Name: ${student.name}\nMark: ${student.mark}\nGrade: ${grade}\n`);
+    totalMarks += student.mark
+  }
+
+  const averageMarks = totalMarks / students.length;
+  return averageMarks;
+}
+
+Average()
+
